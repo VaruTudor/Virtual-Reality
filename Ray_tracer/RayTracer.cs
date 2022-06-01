6c09e1d536412dafe17051170a76c3207bd1aa12
Goian Tudor <varutudor@gmail.com>
2022-06-02 01:08:02 +0300

    Added all Labs."

---
+++ Ray_tracer/RayTracer.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace rt
+{
+    class RayTracer
+    {
+        private Geometry[] geometries;
+        private Light[] lights;
+
+        public RayTracer(Geometry[] geometries, Light[] lights)
+        {
+            this.geometries = geometries;
+            this.lights = lights;
+        }
+
+        private double ImageToViewPlane(int n, int imgSize, double viewPlaneSize)
+        {
+            var u = n * viewPlaneSize / imgSize;
+            u -= viewPlaneSize / 2;
+            return u;
+        }
+
+        private Intersection FindFirstIntersection(Line ray, double minDist, double maxDist)
+        {
+            var intersection = new Intersection();
+
+            foreach (var geometry in geometries)
+            {
+                var intr = geometry.GetIntersection(ray, minDist, maxDist);
+
+                if (!intr.Valid || !intr.Visible) continue;
+
+                if (!intersection.Valid || !intersection.Visible)
+                {
+                    intersection = intr;
+                }
+                else if (intr.T < intersection.T)
+                {
+                    intersection = intr;
+                }
+            }
+
+            return intersection;
+        }
+
+        private bool IsLit(Vector point, Light light)
+        {
+            Line ray = new Line(light.Position, point);
+            var intersection = FindFirstIntersection(ray, 0.0, 1000.0);
+            
+            // should return true if the light ray does not intersect other object
+            return (Math.Abs(intersection.Position.X - point.X) < 1) &&
+                   (Math.Abs(intersection.Position.Y - point.Y) < 1) &&
+                   (Math.Abs(intersection.Position.Z - point.Z) < 1);
+        }
+        
+        private Color GetColorUsingLight(Camera camera, Intersection intersection, Light light)
+        {
+            
+            var material = intersection.Geometry.Material;
+            
+            var color = material.Ambient * light.Ambient;
+            
+            if (IsLit(intersection.Position, light))
+            {
+                var n = (intersection.Position - ((Sphere) intersection.Geometry).GetCenter()).Normalize();
+                var t = (light.Position - intersection.Position).Normalize();
+            
+                if (n*t > 0)
+                {
+                    color += material.Diffuse * light.Diffuse * (n*t);
+                }
+
+                var e = (camera.Position - intersection.Position).Normalize();
+                var r = n * (n * t) * 2 - t;
+
+                if (e*r > 0)
+                {
+                    color += material.Specular * light.Specular * Math.Pow((e * r), material.Shininess);
+                }
+
+                color *= light.Intensity;
+            }
+            
+            return color;
+        }
+        
+        public void Render(Camera camera, int width, int height, string filename)
+        {
+            var image = new Image(width, height);
+
+            for (var i = 0; i < width; i++)
+            {
+                for (var j = 0; j < height; j++)
+                {
+                    var x0 = camera.Position;
+                    var x1 = camera.Position +
+                        camera.Direction * camera.ViewPlaneDistance +
+                        (camera.Up ^ camera.Direction) * ImageToViewPlane(i, width, camera.ViewPlaneWidth) +
+                        camera.Up * ImageToViewPlane(j, height, camera.ViewPlaneHeight);
+                    var intersection = FindFirstIntersection(new Line(x0, x1), camera.FrontPlaneDistance, camera.BackPlaneDistance);
+                    if (intersection.Geometry == null)
+                    {
+                        image.SetPixel(i, j, new Color());
+                        
+                    }
+                    else
+                    {
+                        //check if there is light
+                        var pixelColor = new Color();
+                        pixelColor = lights.Aggregate(pixelColor, (current, light) => current + GetColorUsingLight(camera, intersection, light));
+                        image.SetPixel(i, j, pixelColor);
+                    }
+                }
+            }
+
+            image.Store(filename);
+        }
+    }
+}
---
+++ Ray_tracer/Sphere.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace rt
+{
+    public class Sphere : Geometry
+    {
+        private Vector Center { get; set; }
+        private double Radius { get; set; }
+
+        public Sphere(Vector center, double radius, Material material, Color color) : base(material, color)
+        {
+            Center = center;
+            Radius = radius;
+        }
+
+        public Vector GetCenter()
+        {
+            return Center;
+        }
+        
+        /**
+         * http://www.ambrsoft.com/TrigoCalc/Sphere/SpherLineIntersection_.htm
+         * https://www.youtube.com/watch?v=HFPlKQGChpE - ray intersection with sphere
+         */
+        public override Intersection GetIntersection(Line line, double minDist, double maxDist)
+        {
+            // t denotes the time where the ray is closest to the center of the sphere (perpendicular)
+            // also if the ray intersects the sphere in two points, both are equally distance from t and we'll call
+            // the time it takes to reach them t1 and t2
+            var t = (Center - line.X0) * line.Dx;
+            
+            // p denotes the point on the ray after time t
+            var p = line.CoordinateToPosition(t);
+
+            // y denotes the lenght between the center of the sphere and p
+            var y = (Center - p).Length();
+
+            // x is the distance between p and p1,p2 - the points reached after t1 and t2
+            var x = Math.Sqrt(Radius * Radius - y * y);
+        
+            // we are only interested in the first intersection with the sphere - the minimal t
+            // if x is 0 then t1 is t
+            var t1 = t - x;
+            
+            if (t > minDist && t < maxDist && y <= Radius)
+            {
+                return new Intersection(true, true, this, line, t1);
+            }
+            return new Intersection();
+        }
+
+        public override Vector Normal(Vector v)
+        {
+            var n = v - Center;
+            n.Normalize();
+            return n;
+        }
+    }
+}
